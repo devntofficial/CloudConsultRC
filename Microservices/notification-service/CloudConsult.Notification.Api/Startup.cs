0db5b70442c7cb2850bccfea81758e6afeeb10d4
mukul.verma4 <mukul.verma4@globallogic.com>
2021-11-27 15:40:24 +0530

    notification

--- Microservices/notification-service/CloudConsult.Notification.Api/Startup.cs
+++ Microservices/notification-service/CloudConsult.Notification.Api/Startup.cs
@@ -2,19 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CloudConsult.Notification.Common.Configuration;
+using CloudConsult.Notification.Infrastructure.Consumers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace CloudConsult.Notification.Api
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            KafkaConsumerConfiguration kafkaConsumerConfiguration = new KafkaConsumerConfiguration();
+            _configuration.Bind(nameof(KafkaConsumerConfiguration), kafkaConsumerConfiguration);
+            services.AddHostedService<DoctorUpdateConsumer>();
+            services.AddSingleton(kafkaConsumerConfiguration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
