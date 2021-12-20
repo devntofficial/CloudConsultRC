62b8040dfe729dd564479684f64a7fd7a198dc7c
Nikhil Thakral <devntofficial@gmail.com>
2021-12-20 13:11:33 +0530

    docker container for api gateway

--- ApiGateway/CloudConsult.ApiGateway/Program.cs
+++ ApiGateway/CloudConsult.ApiGateway/Program.cs
@@ -1,7 +1,29 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Prometheus;
+using Prometheus.SystemMetrics;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration as IConfiguration;
+
+// Serilog setup
+var elasticSearchServers = config["ElasticsearchServers"].Split(',').Select(x => new Uri(x));
+builder.Host
+    .UseSerilog((context, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .WriteTo.Console()
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(elasticSearchServers)
+    {
+        IndexFormat = $"{config["ApiName"]}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.Now:MM-yyyy}",
+        AutoRegisterTemplate = true,
+        NumberOfShards = 2,
+        NumberOfReplicas = 1
+    }));
+
 //add gateway configuration
 builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
 {
@@ -13,12 +35,18 @@ builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
         .AddEnvironmentVariables();
 }).ConfigureServices(s =>
 {
+    s.AddSystemMetrics();
     s.AddOcelot();
-}).ConfigureLogging((hostingContext, logging) =>
-{
-    logging.AddConsole();
-});
+})
+;
 var app = builder.Build();
+
+app.UseRouting();
+app.UseHttpMetrics();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapMetrics();
+});
 
 app.UseOcelot().Wait();
 
